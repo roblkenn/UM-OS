29765c7ac162b7a9dd7bc7aca41454dfe260e07c
Nicholas Wilson <nwilsonj@umich.edu>
2018-10-13 16:41:58 -0400

    Basic Exception Level functionality

--- build/MiniUart_c.d
+++
@@ -1,3 +0,0 @@
-build/MiniUart_c.o: src/MiniUart.c include/utils.h \
- include/peripherals/MiniUart.h include/peripherals/base.h \
- include/peripherals/gpio.h
--- src/kernel.c
+++ src/kernel.c
@@ -1,9 +1,15 @@
 #include "MiniUart.h"
+#include "utils.h"
 
 void kernelMain(void) {
     uartInit();
 
-    uartSendString("Hello, Kernel!\r\n");
+    uartSendString("Current Exception level: ");
+    int el = getEL();
+    uartSend('0'+el);
+    uartSendString("\r\n");
+
+    uartSendString("Managed to avoid drowning!\r\n");
 
     while (1) {
         uartSend(uartRecv());
