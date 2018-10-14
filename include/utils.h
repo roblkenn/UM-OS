566207965b8ff60131a8ccccb6ad1d05e1cdead8
Nicholas Wilson <nwilsonj@umich.edu>
2018-10-13 21:53:55 -0400

    Added descriptions for a couple functions

--- include/utils.h
+++ include/utils.h
@@ -1,9 +1,18 @@
 #ifndef     _BOOT_H
 #define     _BOOT_H
 
+/*
+*Delay given amount of cycles (not time)
+*/
 extern void delay(unsigned long);
+
 extern void put32(unsigned long, unsigned int);
 extern unsigned int get32(unsigned long);
+
+/*
+*Returns the current process' exception level
+*Should be 1 for OS level, 0 for Application level
+*/
 extern int getEL();
 
 #endif
